144ea782fd07ce79b404c44bd669c5194ba2aec1
Ruchitha_Gowda <ruchithagowda242@gmail.com>
2024-10-27 16:02:54 +0530

    final UI updated

--- frontend/src/form.jsx
+++ frontend/src/form.jsx
@@ -1,5 +1,6 @@
 import React, { useState } from 'react';
 import axios from 'axios';
+import './app.css'
 
 const Form = () => {
     // Define state for each feature
@@ -46,104 +47,120 @@ const Form = () => {
     };
 
     return (
-        <div>
-            <h1>Heart Disease Prediction</h1>
-            <form onSubmit={handleSubmit}>
-                <input
-                    type="number"
-                    placeholder="Age"
-                    value={age}
-                    onChange={(e) => setAge(e.target.value)}
-                    required
-                />
-                <input
-                    type="number"
-                    placeholder="Sex (0 or 1)"
-                    value={sex}
-                    onChange={(e) => setSex(e.target.value)}
-                    required
-                />
-                <input
-                    type="number"
-                    placeholder="Chest Pain Type (cp)"
-                    value={cp}
-                    onChange={(e) => setCp(e.target.value)}
-                    required
-                />
-                <input
-                    type="number"
-                    placeholder="Resting Blood Pressure (trestbps)"
-                    value={trestbps}
-                    onChange={(e) => setTrestbps(e.target.value)}
-                    required
-                />
-                <input
-                    type="number"
-                    placeholder="Cholesterol (chol)"
-                    value={chol}
-                    onChange={(e) => setChol(e.target.value)}
-                    required
-                />
-                <input
-                    type="number"
-                    placeholder="Fasting Blood Sugar (fbs)"
-                    value={fbs}
-                    onChange={(e) => setFbs(e.target.value)}
-                    required
-                />
-                <input
-                    type="number"
-                    placeholder="Resting Electrocardiographic Results (restecg)"
-                    value={restecg}
-                    onChange={(e) => setRestecg(e.target.value)}
-                    required
-                />
-                <input
-                    type="number"
-                    placeholder="Maximum Heart Rate Achieved (thalach)"
-                    value={thalach}
-                    onChange={(e) => setThalach(e.target.value)}
-                    required
-                />
-                <input
-                    type="number"
-                    placeholder="Exercise Induced Angina (exang)"
-                    value={exang}
-                    onChange={(e) => setExang(e.target.value)}
-                    required
-                />
-                <input
-                    type="number"
-                    placeholder="Old Peak"
-                    value={oldpeak}
-                    onChange={(e) => setOldpeak(e.target.value)}
-                    required
-                />
-                <input
-                    type="number"
-                    placeholder="Slope of Peak Exercise ST Segment (slope)"
-                    value={slope}
-                    onChange={(e) => setSlope(e.target.value)}
-                    required
-                />
-                <input
-                    type="number"
-                    placeholder="Number of Major Vessels (ca)"
-                    value={ca}
-                    onChange={(e) => setCa(e.target.value)}
-                    required
-                />
-                <input
-                    type="number"
-                    placeholder="Thalassemia (thal)"
-                    value={thal}
-                    onChange={(e) => setThal(e.target.value)}
-                    required
-                />
-                <button type="submit">Predict</button>
-            </form>
+        <div id='form-comp'>
+            <h1>Heart Stroke Prediction</h1>
+            <section id="form-section">
+                <form onSubmit={handleSubmit} id='form'>
+                    <div id="fields">
+                        <input
+                            type="number"
+                            placeholder="Age"
+                            value={age}
+                            onChange={(e) => setAge(e.target.value)}
+                            required
+                        />
+                        <input
+                            type="number"
+                            placeholder="Sex (0 or 1)"
+                            value={sex}
+                            onChange={(e) => setSex(e.target.value)}
+                            required
+                        />
+                    </div>
+                    <div id="fields">
+                    <input
+                            type="number"
+                            placeholder="Chest Pain Type (cp)"
+                            value={cp}
+                            onChange={(e) => setCp(e.target.value)}
+                            required
+                        />
+                        <input
+                            type="number"
+                            placeholder="Resting Blood Pressure (trestbps)"
+                            value={trestbps}
+                            onChange={(e) => setTrestbps(e.target.value)}
+                            required
+                        />
+                    </div>
+                    <div id="fields">
+                    <input
+                            type="number"
+                            placeholder="Cholesterol (chol)"
+                            value={chol}
+                            onChange={(e) => setChol(e.target.value)}
+                            required
+                        />
+                        <input
+                            type="number"
+                            placeholder="Fasting Blood Sugar (fbs)"
+                            value={fbs}
+                            onChange={(e) => setFbs(e.target.value)}
+                            required
+                        />
+                    </div>
+                    <div id="fields">
+                    <input
+                            type="number"
+                            placeholder="Resting Electrocardiographic Results (restecg)"
+                            value={restecg}
+                            onChange={(e) => setRestecg(e.target.value)}
+                            required
+                        />
+                        <input
+                            type="number"
+                            placeholder="Maximum Heart Rate Achieved (thalach)"
+                            value={thalach}
+                            onChange={(e) => setThalach(e.target.value)}
+                            required
+                        />
+                    </div>
+                    <div id="fields">
+                    <input
+                            type="number"
+                            placeholder="Exercise Induced Angina (exang)"
+                            value={exang}
+                            onChange={(e) => setExang(e.target.value)}
+                            required
+                        />
+                        <input
+                            type="number"
+                            placeholder="Old Peak"
+                            value={oldpeak}
+                            onChange={(e) => setOldpeak(e.target.value)}
+                            required
+                        />
+                    </div>
+                    <div id="fields">
+                    <input
+                            type="number"
+                            placeholder="Slope of Peak Exercise ST Segment (slope)"
+                            value={slope}
+                            onChange={(e) => setSlope(e.target.value)}
+                            required
+                        />
+                        <input
+                            type="number"
+                            placeholder="Number of Major Vessels (ca)"
+                            value={ca}
+                            onChange={(e) => setCa(e.target.value)}
+                            required
+                        />
+                    </div>
+                    <div id="fields">
+                    <input
+                            type="number"
+                            placeholder="Thalassemia (thal)"
+                            value={thal}
+                            onChange={(e) => setThal(e.target.value)}
+                            required
+                        />
+                        <button id='pred-button' type="submit">Predict</button>
+                    </div>
+                </form>
+            </section>
 
-            {prediction !== null && <h2>Prediction: {prediction}</h2>}
+            {prediction !== null && <h2 id='results'>{prediction==1? "Potential risk of heart stroke": "You're safe"}</h2>}
             {error && <p>{error}</p>}
         </div>
     );
